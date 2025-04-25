6f8775b773c86d7a4645e2d60c2dc9d5f6ce88d4
HackerB777 <jokersiva46@gmail.com>
2025-04-25 11:23:30 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<your-repo-name>/', // Replace <your-repo-name> with your repository name
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
-});
+})
